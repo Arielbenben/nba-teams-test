ee78dcb9b83aab3047e6da9dcc4131ec568dc530
Ariel <relbh18@gmail.com>
2024-09-19 09:26:33 +0300

    start the project

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+
+
+# app = Flask(__name__)
+#
+# if __name__ == "__main__":
+#     app.register_blueprint(user_blueprint, url_prefix="/api/users")
+#     app.register_blueprint(questions_blueprint, url_prefix="/api/questions")
+#     app.run(debug=True)
